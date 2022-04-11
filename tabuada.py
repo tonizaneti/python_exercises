480eb65e72bec98390a5d1dc22eb4343101eec0a
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2022-04-11 18:54:41 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,11 @@
+valor = int (input('Entre com um número para saber a tabuada:'))
+aux=0
+
+print('*' * 18)
+
+print('Tabuada de {} '. format(valor))
+print('*' * 18)
+
+while(aux <=10):
+    print('{0} x {1} = {2}'.format(aux, valor, (aux * valor)))
+    aux = aux+1
